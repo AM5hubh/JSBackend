2786e10b7693771b98878d504e390a030005880d
AM5hubh <ssahamate2020@gmail.com>
2025-02-23 20:50:12 +0530

    commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,27 +1,31 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import {errorHandler} from "./middlewares/errorHandler.js"
-const app = express()
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorHandler } from "./middlewares/errorHandler.js";
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('public'))
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 //routes import
 
-import userRouter from './routes/user.routes.js'
+import userRouter from "./routes/user.routes.js";
 
 //routes declaration
-app.use("/api/v1/users", userRouter) // goes to user.routes.js
+app.use("/api/v1/users", userRouter); // goes to user.routes.js
 //http://localhost:8000/api/v1/users/register
+app.get("/healthz", (req, res) => {
+  res.status(200).send("OK");
+});
 
 app.use(errorHandler);
-export {app}
+export { app };
