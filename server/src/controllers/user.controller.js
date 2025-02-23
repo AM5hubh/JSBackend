1b928f7d533571ff3b3c4c8ae4b8c41c4a177247
AM5hubh <ssahamate2020@gmail.com>
2025-02-23 18:29:19 +0530

    error handling check 2

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -104,7 +104,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const { email, username, password } = req.body;
   if (!email && !username) {
-    return res.json(ApiError(400, "Username or email required"));
+    return res.json(new ApiError(400, "Username or email required"));
   }
 
   const user = await User.findOne({
