22111dae38a4693762e267293cd314627475afdd
AM5hubh <ssahamate2020@gmail.com>
2025-03-09 21:20:31 +0530

    error resolved

--- server/src/app.js
+++ server/src/app.js
@@ -4,21 +4,27 @@ import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/errorHandler.js";
 const app = express();
 
-// const allowedOrigins = [
-//   `${process.env.CORS_ORIGIN1}`,
-//   `${process.env.CORS_ORIGIN2}`,
-//   `${process.env.CORS_ORIGIN3}`,
-//   `${process.env.CORS_ORIGIN4}`,
-//   `${process.env.CORS_ORIGIN5}`,
-// ];
+const allowedOrigins = [
+  `${process.env.CORS_ORIGIN1}`,
+  `${process.env.CORS_ORIGIN2}`,
+  `${process.env.CORS_ORIGIN3}`,
+  `${process.env.CORS_ORIGIN4}`,
+  `${process.env.CORS_ORIGIN5}`,
+];
 
 app.use(
   cors({
-      origin: process.env.CORS_ORIGIN,
+      origin: (origin, callback) => {
+          if (allowedOrigins.includes(origin) || !origin) {
+              callback(null, true);
+          } else {
+              callback(new Error('Not allowed by CORS'));
+          }
+      },
       credentials: true,
   })
 );
-// app.disable("x-powered-by");
+app.disable("x-powered-by");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('public'))
